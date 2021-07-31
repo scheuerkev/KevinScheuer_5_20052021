b40c8af5f07e83685d751d83d25aa9ac370424d0
Scheuer Kévin <scheuer.kevin@gmail.com>
2021-07-31 16:38:48 +0200

    Add comments on main.js and catch blocks

--- public/js/main.js
+++ public/js/main.js
@@ -1,31 +1,51 @@
 loadConfig().then(data => {
-    config = data;
-    fetch(config.host + "/api/furniture")        //Fetching API
-        .then(data => data.json())                     //Promise return raw datas parsed from JSON
-        .then(values => {                              //Got a JS Object now
-
-            for (let value of values) {                //Building new product from Product class for each value in values :D
+    console.log("Host config loaded");
+    //Fetching API
+    fetch(data.host + "/api/furniture")
+        //Promise return raw datas parsed from JSON
+        .then(data => {
+            console.log("API connexion OK");
+            return data.json();
+        })
+        //Got a promise with JS Object now
+        .then(values => {
+            console.log("Get results from API");
+            for (let value of values) {
+                //Building new product from Product class for each value in values array returned
                 let product = new Product(value);
+                //Adding a product card in products html section
                 document
                     .getElementById('products')
                     .innerHTML += product.displayAllProducts(value);
             }
 
             let hero = document.getElementsByClassName('hero');
-            hero[0].classList.remove('invisible'); //This remove invisible class to hero class be only shown on first product
+            //Remove invisible class to hero class be only shown on first product
+            hero[0].classList.remove('invisible');
 
 
         })
         .catch(err => {
+            //Catch and throw error of fetching API promise
+            console.error("API error : " + err);
             document
                 .getElementById('products')
                 .innerHTML = `<div class="error"> 
                             <i class="fas fa-plug"></i>
-                            <p>Help ! Quelqu'un a débranché une prise !</p>
+                            <p>Help ! Il semble qu'il y ait un problème avec le serveur !</p>
                             </div>`;
-            throw err;                                        //Throw error !
         });
-});
+    })
+    .catch(err => {
+        //Catch and throw error of fetching config.json promise
+        console.error("Host config error : " + err);
+        document
+            .getElementById('products')
+            .innerHTML = `<div class="error"> 
+                            <i class="fas fa-plug"></i>
+                            <p>Help ! Il semble qu'il y ait un problème avec la configuration du serveur !</p>
+                            </div>`;
+    });
 
 
 
